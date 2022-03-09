77e282411e20f8d2ba77694261f75ac8c1679441
Gaetan Manchon <gmanchon@gmail.com>
2022-03-09 14:18:58 +0100

    simplify image display

--- app.py
+++ app.py
@@ -30,15 +30,18 @@ if uploaded_file is not None:
             with open(local_filename, "wb") as f:
                 shutil.copyfileobj(response.raw, f)
 
-            # display image https://discuss.streamlit.io/t/how-to-show-local-gif-image/3408/3
-            with open(local_filename, "rb") as f:
+            # display image
+            st.image(local_filename)
 
-                content = f.read()
+            # # display image https://discuss.streamlit.io/t/how-to-show-local-gif-image/3408/3
+            # with open(local_filename, "rb") as f:
 
-                data_url = base64.b64encode(content).decode("utf-8")
-                st.markdown(
-                    f'<img src="data:image/gif;base64,{data_url}" alt="the gif">',
-                    unsafe_allow_html=True)
+            #     content = f.read()
+
+            #     data_url = base64.b64encode(content).decode("utf-8")
+            #     st.markdown(
+            #         f'<img src="data:image/gif;base64,{data_url}" alt="the gif">',
+            #         unsafe_allow_html=True)
 
         else:
             "😬 api error 🤖"
